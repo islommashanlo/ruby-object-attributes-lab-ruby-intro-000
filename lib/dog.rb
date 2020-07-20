d7fbb4b4579158ab34edf664d3dce5eca4b44302
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-20 15:50:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,3 +3,19 @@ class Dog
     @name = name
     @breed = breed
   end
+
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def breed=(breed_name)
+    @breed = breed_name
+  end
